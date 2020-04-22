9f2f182039015bdb0081d5de0685b6457ae7da71
ZhenFu <zhfu@silabs.com>
2020-04-22 15:39:59 +0800

    add TODO to the lab.h

--- lab/lab.h
+++ lab/lab.h
@@ -9,6 +9,24 @@ extern "C"
 #include "logging/log.h"
 #include "models/sensor_server.h"
 
+/*
+ * TODO:
+ *
+ * Check how the difference in regulating the lightness between uncommenting and
+ * commenting the below line.
+ *
+ *  - Comment the below line out - the sensor server won't send ambient light
+ *    level data to the LC server. So, the LC server will only use the lightness
+ *    settings to regulate the lightness of the lightbulbs.
+ *  - Uncomment the below line - the sensor server will loopback the ambient
+ *    light level data to the LC server, the PI regulator will output an
+ *    expected lightness, the real lightness output is the higher value of the
+ *    PI regulator output and the lightness setting.
+ *
+ * NOTE: the LC properties are stored in flash, when you change the below line,
+ * you need to update the stored value as well. The easiest way for this lab
+ * could be factory reset and re-provisioning.
+ */
 #define AMBIENT_LUX_CTL
 
 #define SENSOR_SERVER_MODEL_ELEMENT_INDEX 0
@@ -16,6 +34,7 @@ extern "C"
 void self_config(const struct gecko_msg_mesh_node_initialized_evt_t *evt);
 void on_appkey_added(const struct gecko_msg_mesh_node_key_added_evt_t *evt);
 void on_node_initialized(const struct gecko_msg_mesh_node_initialized_evt_t *evt);
+
 #ifdef __cplusplus
 }
 #endif
