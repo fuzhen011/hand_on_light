4c0c7d6c615316c88f9aeb7b62afb72955cde8cb
ZhenFu <zhfu@silabs.com>
2020-04-23 21:31:57 +0800

    modify the default value for lc ambient properties

--- light_controller.c
+++ light_controller.c
@@ -267,10 +267,10 @@ static int lc_property_state_load(void)
     lc_property_state.ambient_luxlevel_prolong = 550;
     lc_property_state.ambient_luxlevel_standby = 150;
 #endif
-    lc_property_state.regulator_kiu = 6.0;
-    lc_property_state.regulator_kid = 60.0;
-    lc_property_state.regulator_kpu = 40.0;
-    lc_property_state.regulator_kpd = 40.0;
+    lc_property_state.regulator_kiu = 200.0;
+    lc_property_state.regulator_kid = 200.0;
+    lc_property_state.regulator_kpu = 90.0;
+    lc_property_state.regulator_kpd = 90.0;
     lc_property_state.regulator_accuracy = 4;
     return -1;
   }
